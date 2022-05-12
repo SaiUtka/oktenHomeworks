3c36f58032899155bc7d673cb52bac2917e59239
SaiUtka <smnddk@ukr.net>
2022-05-12 15:42:07 +0300

    hw done

---
+++ homeworks/exrta homework/lesson 8/script/script.js
@@ -0,0 +1,22 @@
+// Написати рекурсивну функцію, яка збирає всі назви класів з файлу rules.html в окремий масив.
+// масив вивести в консоль
+
+let res = []
+let fn = (item) => {
+    let children = item.children;
+    for (const child of children) {
+        if (child.classList.value) {
+            res.push(child.classList.value)
+        }
+        fn(child);
+    }
+    return res
+};
+let content = document.body
+console.log(fn(content));
+
+
+
+
+
+
